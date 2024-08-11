37bf0c23576d7eb1c78b9ab364e3e9a31dfcae28
Luanzacarias <luanzacarias215@gmail.com>
2024-08-11 17:59:42 -0300

    feat: indicacao de loading

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,15 +1,16 @@
 import { ViewState } from "@devexpress/dx-react-scheduler";
-import { Appointments, DateNavigator, DayView, MonthView, Scheduler, Toolbar, ViewSwitcher, WeekView, CurrentTimeIndicator, AppointmentTooltip } from "@devexpress/dx-react-scheduler-material-ui";
+import { Appointments, DateNavigator, DayView, MonthView, Scheduler, Toolbar, ViewSwitcher, WeekView, CurrentTimeIndicator, AppointmentTooltip, TodayButton } from "@devexpress/dx-react-scheduler-material-ui";
 import { Paper, useTheme } from "@mui/material";
 import { useState } from "react";
 import { AppointmentComponent, AppointmentTooltipContent } from "./Appointment";
+import { ToolbarWithLoading } from "./Toolbar";
 
 
 export function Calendar() {
   
   const [date, setDate] = useState(new Date());
   const {palette} = useTheme();
-
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <Paper style={{backgroundColor: palette.background.default}} >
@@ -17,7 +18,7 @@ export function Calendar() {
       <Scheduler
         locale="pt-BR"
         height={window.innerHeight*0.9}
-        firstDayOfWeek={1}
+        firstDayOfWeek={0}
         data={[
           { title: 'Mail New Leads for Follow Up', startDate: '2024-08-05T10:00', bg: "#F0f" },
           { title: 'Product Meeting', startDate: '2024-08-05T10:30', endDate: '2024-08-05T11:30', bg: "#80f", description: "Um texto bonito de descrição da atividade" },
@@ -29,9 +30,12 @@ export function Calendar() {
           currentDate={date}
           onCurrentDateChange={setDate}
         />
-        <Toolbar />
+        <Toolbar 
+          {...isLoading ? { rootComponent: ToolbarWithLoading } : null }
+        />
         <ViewSwitcher />
         <DateNavigator />
+        <TodayButton />
         <WeekView
           displayName="Semana"
           cellDuration={30}
