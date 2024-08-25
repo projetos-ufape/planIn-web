66ce1b4c59bfc749a42338f1d15155a7186cb7bb
Luanzacarias <luanzacarias215@gmail.com>
2024-08-25 10:52:23 -0300

    docs: remocao de texto readme

--- src/components/ComponenteExemplo/styles.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from "react";
-
-const estiloExemplo: CSSProperties = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-export default { estiloExemplo };
